ee2b587833c4270b50afea4ad831fbf44ce831b8
Levi <44460654+LevisAlone@users.noreply.github.com>
2020-09-04 15:31:55 -0700

    prettify files

--- HideFlags/script.js
+++ HideFlags/script.js
@@ -1,20 +1,20 @@
 function updateOutput() {
-    var output = document.getElementById("output");
-    var code = document.getElementsByClassName("code")[0];
-	var amount = 0;
-	for (var i = 1; i <= 64; i *= 2)
-    if(document.getElementById(i.toString()).checked)
-        amount += i;
-    if(amount != 0) {
-        output.value = "HideFlags:"+amount;
-        code.innerHTML = `/give @s minecraft:stone{HideFlags:${amount}}`;
-    }
-    else {
-        // HideFlags:0 is invalid, and I don't want to give anyone the impression they can use it.
-        output.value = "";
-        code.innerHTML = `/give @s minecraft:stone{HideFlags:&lt;Bit Mask&gt;}`;
-    }
-    
+  var output = document.getElementById("output");
+  var code = document.getElementsByClassName("code")[0];
+  var amount = 0;
+
+  for (var i = 1; i <= 64; i *= 2)
+    if (document.getElementById(i.toString()).checked)
+      amount += i;
+  
+  if (amount != 0) {
+    output.value = "HideFlags:" + amount;
+    code.innerHTML = `/give @s minecraft:stone{HideFlags:${amount}}`;
+  } else {
+    // HideFlags:0 is invalid, and I don't want to give anyone the impression they can use it.
+    output.value = "";
+    code.innerHTML = `/give @s minecraft:stone{HideFlags:&lt;Bit Mask&gt;}`;
+  }
 }
 
 function copy(elementId) {
